ce4d20c20a379cb5c3e6cb947599a8aa8d752921
Crowdbotics <team@crowdbotics.com>
2020-03-18 15:34:01 +0000

    Initial commit.

---
+++ backend/bnnbbn_dev_2057/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bnnbbn_dev_2057 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bnnbbn_dev_2057.settings')
+
+application = get_wsgi_application()
